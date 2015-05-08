c9a8308830510a4f3ce36870d550c1648e2dfe21
kevinsheu <kevin.cp.sheu@gmail.com>
2015-05-08 10:53:33 -0700

    deleted all really

--- gen/com/hackathon/heart2heart/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package com.hackathon.heart2heart;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
