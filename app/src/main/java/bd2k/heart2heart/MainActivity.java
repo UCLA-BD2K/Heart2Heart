39aab63394e8eceb8c3f615e19c0429b2a3bfbfd
hannah <hannahjchou@gmail.com>
2015-05-09 10:32:35 -0700

    added logo to main page

--- app/src/main/java/bd2k/heart2heart/MainActivity.java
+++ app/src/main/java/bd2k/heart2heart/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -15,11 +17,17 @@ public class MainActivity extends ActionBarActivity {
     Button beginRun;
     Button history;
     Button profileInfo;
+    ImageView logoView;
+    TextView welcomeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logoView = (ImageView) findViewById(R.id.imageView);
+        logoView.setAlpha(50);
+        welcomeView = (TextView) findViewById(R.id.textView);
+        welcomeView.bringToFront();
         addListenerOnButtons();
     }
 
